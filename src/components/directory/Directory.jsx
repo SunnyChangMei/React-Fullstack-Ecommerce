5f079b43d05e7f18a750a51a7a29e5e8f5922695
Sunny Chang Mei <mkaleader@gmail.com>
2019-09-21 18:09:13 -0400

    update menu image scss with hover effect

--- src/components/directory/Directory.jsx
+++ src/components/directory/Directory.jsx
@@ -2,43 +2,38 @@ import React, { Component } from 'react';
 
 import './directory.styles.scss';
 import MenuItem from '../menu-item/MenuItem';
+class Directory extends React.Component {
+  constructor() {
+    super();
 
-class Directory extends Component {
-  constructor(props) {
-    super(props);
     this.state = {
       sections: [
         {
           title: 'hats',
           imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-          id: 1,
-          linkUrl: 'shop/hats'
+          id: 1
         },
         {
           title: 'jackets',
           imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-          id: 2,
-          linkUrl: 'shop/jackets'
+          id: 2
         },
         {
           title: 'sneakers',
           imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-          id: 3,
-          linkUrl: 'shop/sneakers'
+          id: 3
         },
         {
           title: 'womens',
           imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
           size: 'large',
-          id: 4,
-          linkUrl: 'shop/womens'
+          id: 4
         },
         {
           title: 'mens',
           imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
           size: 'large',
-          id: 5,
-          linkUrl: 'shop/mens'
+          id: 5
         }
       ]
     };
@@ -47,7 +42,7 @@ class Directory extends Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ id, title, imageUrl, size }) => (
+        {this.state.sections.map(({ title, imageUrl, id, size }) => (
           <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
         ))}
       </div>
