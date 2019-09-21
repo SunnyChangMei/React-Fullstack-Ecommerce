458d90bfda9803749f0c421263db75bcb0b2a158
Sunny Chang Mei <mkaleader@gmail.com>
2019-09-21 17:12:11 -0400

    add HomePage and scss

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './homepage.styles.scss'
+
 const HomePage = () => (
   <div className="homepage">
     <div className="directory-menu">
@@ -13,6 +15,13 @@ const HomePage = () => (
       <div className="menu-item">
         <div className="content">
           <h1 className="title">jackets</h1>
+          <span className="subtitle">shop now</span>
+        </div>
+      </div>
+
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">sneakers</h1>
           <span className="subtitle">shop now</span>
         </div>
       </div>
