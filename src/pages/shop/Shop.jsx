9384d5e80d88848feb885acf8dfa6695fc66a205
Sunny Chang Mei <mkaleader@gmail.com>
2019-09-26 12:36:23 -0400

    add shop.data and component

---
+++ src/pages/shop/Shop.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { SHOP_DATA } from './shop.data';
+import PreviewCollection from '../../components/preview-collection/PreviewCollection';
+
+class ShopPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collections: SHOP_DATA
+    };
+  }
+  render() {
+    const { collections } = this.state;
+    return (
+      <div>
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <PreviewCollection key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
