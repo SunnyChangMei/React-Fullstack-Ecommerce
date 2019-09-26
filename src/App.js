00d380215376bde8ca2cbb7c1ad36857c991991f
Sunny Chang Mei <mkaleader@gmail.com>
2019-09-26 12:35:49 -0400

    add patch to shop

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import HomePage from './pages/homepage/HomePage';
+import { Route, Switch } from 'react-router-dom';
+import ShopPage from './pages/shop/Shop';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path={'/'} component={HomePage} />
+        <Route path={'/shop'} component={ShopPage} />
+      </Switch>
     </div>
   );
 }
