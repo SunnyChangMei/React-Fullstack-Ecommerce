483199ca9031e9030df8d2bef915c7ac1b3e64f0
Sunny Chang Mei <mkaleader@gmail.com>
2019-09-27 12:25:28 -0400

    create SignInSignOut

---
+++ src/pages/sign-in-and-sign-up/SignIn-SignOut.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './sign-in-sign-out.styles.scss';
+import SignIn from '../../components/sign-in/SignIn';
+
+const SignInAndSignUP = () => (
+  <div className="sign-in-and-sign-up"><SignIn/> </div>
+);
+
+export default SignInAndSignUP;
