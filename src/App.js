08288263f0cacfeed619449f2e4b12bd289d09a0
Sunny Chang Mei <mkaleader@gmail.com>
2019-09-29 17:04:12 -0400

    import auth to App

--- src/App.js
+++ src/App.js
@@ -3,21 +3,38 @@ import HomePage from './pages/homepage/HomePage';
 import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/shop/Shop';
 import Header from './components/header/Header';
+import { auth } from './firebase/firebase.utils';
 
-import './App.css'
+import './App.css';
 import SignInAndSignUP from './pages/sign-in-and-sign-up/SignIn-SignOut';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path={'/'} component={HomePage} />
-        <Route path={'/shop'} component={ShopPage} />
-        <Route path={'/signin'} component={SignInAndSignUP} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+      console.log(user);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path={'/'} component={HomePage} />
+          <Route path={'/shop'} component={ShopPage} />
+          <Route path={'/signin'} component={SignInAndSignUP} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
