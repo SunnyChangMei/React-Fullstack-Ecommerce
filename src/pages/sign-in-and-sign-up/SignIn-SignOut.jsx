7097fc65961a235311524811662169d025e74903
Sunny Chang Mei <mkaleader@gmail.com>
2019-10-04 11:13:38 -0400

    import SignUp

--- src/pages/sign-in-and-sign-up/SignIn-SignOut.jsx
+++ src/pages/sign-in-and-sign-up/SignIn-SignOut.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 
 import './sign-in-sign-out.styles.scss';
 import SignIn from '../../components/sign-in/SignIn';
+import SignUp from '../../components/sign-up/SignUp';
 
 const SignInAndSignUP = () => (
-  <div className="sign-in-and-sign-up"><SignIn/> </div>
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+    <SignUp />
+  </div>
 );
 
 export default SignInAndSignUP;
