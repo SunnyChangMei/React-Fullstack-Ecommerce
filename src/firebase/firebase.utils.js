852dd20be414e662c0646a86c568597576dc3aed
Sunny Chang Mei <mkaleader@gmail.com>
2019-10-04 10:09:37 -0400

    createUserProfileDocument in firestore

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,31 @@ const config = {
   measurementId: 'G-22J3T8Z9VP'
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`userAuth/${userAuth.uid}`);
+
+  // get userRef for firestore userAuth
+  const snapShot = await userRef.get();
+
+// check if there is any user data exists, if not, use set.() to create new user with new Date()
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
