9b5f92aa84df325bee6ada4f23e2ffe9aa5d5757
Sunny Mei <mkaleader@gmail.com>
2019-10-17 17:33:21 -0400

    use as='div' in styled component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -13,7 +13,6 @@ import {
   HeaderContainer,
   OptionsContainer,
   OptionLink,
-  OptionDiv,
   LogoContainer
 } from './header.styles';
 
@@ -27,7 +26,7 @@ const Header = ({ currentUser, hidden }) => (
       <OptionLink to="/shop">SHOP</OptionLink>
       <OptionLink to="/contact">CONTACT</OptionLink>
       {currentUser ? (
-        <OptionDiv onClick={() => auth.signOut()}>SIGN OUT</OptionDiv>
+        <OptionLink as='div' onClick={() => auth.signOut()}>SIGN OUT</OptionLink>
       ) : (
         <OptionLink to="/login">SIGN IN</OptionLink>
       )}
