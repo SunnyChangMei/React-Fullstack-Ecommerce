0a0353eeb1a7529d1fd664b59534eb4adc855a44
Sunny Mei <mkaleader@gmail.com>
2019-10-17 14:35:30 -0400

    add check NODE_ENV for dev

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,7 +5,12 @@ import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 
 //middlewares may need to modifies in future, it's better to put it into array [logger], not just logger
-const middlewares = [logger];
+const middlewares = [];
+
+// check if we on dev mode
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
