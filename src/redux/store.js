44784279a1fde11e85678437aa5c9de982dd8ede
Sunny Mei <mkaleader@gmail.com>
2019-10-18 18:08:14 -0400

    create redux saga middleware, move fetchCollectionsAsync to saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,10 +3,14 @@ import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
-import thunk from 'redux-thunk';
+// import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+import rootSage from './root-saga';
+
+const sagaMiddleware = createSagaMiddleware();
 
 //middlewares may need to modifies in future, it's better to put it into array [logger], not just logger
-const middlewares = [thunk];
+const middlewares = [sagaMiddleware];
 
 // check if we on dev mode
 if (process.env.NODE_ENV === 'development') {
@@ -14,6 +18,8 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddleware.run(rootSage);
 
 // create persistStore
 export const persistor = persistStore(store);
