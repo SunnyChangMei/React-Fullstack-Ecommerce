0e02bb197ec855f1e2e3737c3aefcc4fd347481d
Sunny Mei <mkaleader@gmail.com>
2019-10-18 21:20:05 -0400

    create shop.sagas

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,5 +1,4 @@
-import { takeLatest, call, put } from 'redux-saga/effects';
-import ShopActionTypes from './shop.types';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 
 import {
   firestore,
@@ -11,9 +10,9 @@ import {
   fetchCollectionsFailure
 } from './shop.actions';
 
-export function* fetchCollectionsAsync() {
-  yield console.log('I am fired');
+import ShopActionTypes from './shop.types';
 
+export function* fetchCollectionsAsync() {
   try {
     const collectionRef = firestore.collection('collections');
     const snapshot = yield collectionRef.get();
@@ -32,4 +31,8 @@ export function* fetchCollectionsStart() {
     ShopActionTypes.FETCH_COLLECTIONS_START,
     fetchCollectionsAsync
   );
+}
+
+export function* shopSagas() {
+  yield all([call(fetchCollectionsStart)]);
 }
