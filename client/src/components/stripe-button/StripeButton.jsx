55241b777612dbcb26566be4d1fb335fe931068d
Sunny Mei <mkaleader@gmail.com>
2019-10-19 17:36:58 -0400

    import axios and setup route for stripe payment

--- client/src/components/stripe-button/StripeButton.jsx
+++ client/src/components/stripe-button/StripeButton.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publicKey = 'pk_test_MlpDBiQja2GOFRsUTDeegxVT00if8522W6';
 
   const onToken = token => {
-    console.log(token);
-    alert('Payment Successful');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+      .then(response => {
+        alert('Payment successful');
+      })
+      .catch(error => {
+        console.log('Payment error', JSON.parse(error));
+        alert('There was an issue with your payment.');
+      });
   };
 
   return (
