092d8c39f47ec7abaabd77405d7b43fa3055684d
Sunny Mei <mkaleader@gmail.com>
2019-10-20 17:41:30 -0400

    convert collections-overview to graphql

--- client/src/components/collections-overview/collections-overview.container.jsx
+++ client/src/components/collections-overview/collections-overview.container.jsx
@@ -1,18 +1,51 @@
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import { compose } from 'redux';
+// import { connect } from 'react-redux';
+// import { createStructuredSelector } from 'reselect';
+// import { compose } from 'redux';
 
-import { selectCollectionFetching } from '../../redux/shop/shop.selectors';
-import WithSpinner from '../with-spinner/WithSpinner';
+// import { selectCollectionFetching } from '../../redux/shop/shop.selectors';
+// import WithSpinner from '../with-spinner/WithSpinner';
+// import CollectionsOverview from './CollectionsOverview';
+
+// const mapStateToProps = createStructuredSelector({
+//   isLoading: selectCollectionFetching
+// });
+
+// const CollectionsOverviewContainer = compose(
+//   connect(mapStateToProps),
+//   WithSpinner
+// )(CollectionsOverview);
+
+// export default CollectionsOverviewContainer;
+
+import React from 'react';
+import { Query } from 'react-apollo';
+import { gql } from 'apollo-boost';
+
 import CollectionsOverview from './CollectionsOverview';
+import Spinner from '../spinner/spinner.component';
 
-const mapStateToProps = createStructuredSelector({
-  isLoading: selectCollectionFetching
-});
+const GET_COLLECTIONS = gql`
+  {
+    collections {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
 
-const CollectionsOverviewContainer = compose(
-  connect(mapStateToProps),
-  WithSpinner
-)(CollectionsOverview);
+const CollectionsOverviewContainer = () => (
+  <Query query={GET_COLLECTIONS}>
+    {({ loading, data }) => {
+      if (loading) return <Spinner />;
+      return <CollectionsOverview collections={data.collections} />;
+    }}
+  </Query>
+);
 
 export default CollectionsOverviewContainer;
