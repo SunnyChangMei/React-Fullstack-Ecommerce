8abf5d18fc8d23ba1b52ce3346f6e6b73856185b
Sunny Mei <mkaleader@gmail.com>
2019-10-20 17:42:09 -0400

    covert Collection and container, Shop to graphql

--- client/src/pages/shop/Shop.jsx
+++ client/src/pages/shop/Shop.jsx
@@ -4,8 +4,8 @@ import { connect } from 'react-redux';
 
 import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 
-import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
-import CollectionPageContainer from '../collection/collection.container';
+import { default as CollectionsOverview } from '../../components/collections-overview/collections-overview.container';
+import { default as CollectionPage } from '../collection/collection.container';
 
 class ShopPage extends React.Component {
   componentDidMount() {
@@ -18,14 +18,10 @@ class ShopPage extends React.Component {
 
     return (
       <div className="shop-page">
-        <Route
-          exact
-          path={`${match.path}`}
-          component={CollectionsOverviewContainer}
-        />
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
         <Route
           path={`${match.path}/:collectionId`}
-          component={CollectionPageContainer}
+          component={CollectionPage}
         />
       </div>
     );
