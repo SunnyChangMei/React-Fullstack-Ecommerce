df6264c7f27723defa9ee78890a105943b378ccf
Sunny Mei <mkaleader@gmail.com>
2019-10-19 21:36:21 -0400

    fix payment and reinstall all packages

--- client/src/components/stripe-button/StripeButton.jsx
+++ client/src/components/stripe-button/StripeButton.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publicKey = 'pk_test_MlpDBiQja2GOFRsUTDeegxVT00if8522W6';
 
   const onToken = token => {
-    console.log(token);
-    alert('Payment Successful');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+      .then(response => {
+        alert('successful payment');
+      })
+      .catch(error => {
+        console.error(error);
+        alert(
+          'There was an issue with your payment! Please make sure you use the provided credit card.'
+        );
+      });
   };
 
   return (
